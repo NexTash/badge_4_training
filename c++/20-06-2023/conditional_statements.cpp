c81669d60268ac2b43bed4d46da3fd851495dd2e
Faisal Zahoor <faisal-zahoor>
2023-06-21 14:49:32 +0500

    feat: if conditon flowchart

---
+++ c++/20-06-2023/conditional_statements.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Conditional Statments"<<endl;
+    int x, y;
+
+   
+    cout<<"first number";
+    cin>>x;
+    cout<<"second number";
+    cin>>y;
+    
+    if(x==y){
+        cout<<"Sabh sahi ay";
+    }else{
+        cout<<"Mamla garbar hay";
+    }
+    
+    cout<<endl<<endl;
+    
+    return 0;
+}
---
+++ c++/21-06-2023/if-condition-flowchart.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"     (START)    "<<endl;
+    cout<<"        |         "<<endl;
+    cout<<"        |         "<<endl;
+    cout<<"        V         "<<endl;
+    cout<<"        /\\        "<<endl;
+    cout<<"       /  \\       "<<endl;
+    cout<<"      /    \\       "<<endl;
+    cout<<"     /      \\      "<<endl;
+    cout<<"    <         > ---------FALSE-------> (END)"<<endl;
+    cout<<"     \\ cond /       "<<endl;
+    cout<<"      \\    /       "<<endl;
+    cout<<"       \\  /       "<<endl;
+    cout<<"        \\/       "<<endl;
+    cout<<"        |         "<<endl;
+    cout<<"        |         "<<endl;
+    cout<<"       TRUE         "<<endl;
+    cout<<"        V         "<<endl;
+    cout<<"------------------"<<endl;
+    cout<<"|     Statment   |"<<endl;
+    cout<<"------------------"<<endl;
+    cout<<"        |         "<<endl;
+    cout<<"        |         "<<endl;
+    cout<<"        V         "<<endl;
+    cout<<"      (END)         "<<endl;
+    
+    return 0;
+}
